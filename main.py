4d4aff2c5b800d7ab68048210230d0011c10b001
Cristian Widenhouse <cgwidenhouse@gmail.com>
2018-06-23 12:28:23 -0400

    rewrite present_actions, various formatting, ensure that update_the_picker works properly

--- main.py
+++ main.py
@@ -8,61 +8,11 @@ queue_sel = 'SELECT title FROM Movies WHERE picker_ID = ? AND watched = 0;'
 queue_ins = 'INSERT INTO Movies VALUES (NULL, ?, ?, 0);'
 queue_del = 'DELETE FROM Movies WHERE title = ?;'
 queue_watch = 'UPDATE Movies SET watched = 1 WHERE title = ?;'
-watched_list = 'SELECT title, name FROM Movies INNER JOIN Members ON picker_ID \
-                = member_ID WHERE watched = 1;'
+watched_list = 'SELECT title, name FROM Movies INNER JOIN Members ON picker_ID\
+                 = member_ID WHERE watched = 1;'
 club_members = 'SELECT name FROM Members;'
 conn = connect('movie_club.db')
 c = conn.cursor()
-
-
-def main():
-    with open('greeting_string.txt') as file:
-        greeting = file.read()
-    print('\n\n\n\n\n' + greeting)
-    present_actions()
-
-
-def present_actions():
-    actions = ['View Movie Queues', 'Add to a Queue', 'Delete from a Queue',
-               'Add Movie to Watched List', 'View Watched List',
-               'Delete from Watched List', 'View Club Members',
-               'Whose Turn Is It?', 'TEST', 'Exit the Program']
-    menu = '\n\nSelect an Action:\n-----------------\n'
-    number_of_actions = 10
-    for i in range(number_of_actions):
-        menu += str(i+1) + ' - ' + actions[i] + '\n\n'
-    menu += '\nAction:'
-    print(menu)
-    try:
-        action = int(input())
-        if action not in range(1, number_of_actions+1):
-            raise ValueError
-        if action == 10:
-            print('Goodbye!')
-            conn.commit()
-            conn.close()
-            return
-        elif action == 1:
-            view_movie_queues()
-        elif action == 2:
-            add_to_queue()
-        elif action == 3:
-            delete_from_queue()
-        elif action == 4:
-            add_to_watched()
-        elif action == 5:
-            view_watched()
-        elif action == 6:
-            delete_from_watched()
-        elif action == 7:
-            view_club_members()
-        elif action == 8:
-            show_club_status()
-        elif action == 9:
-            update_the_picker()
-    except ValueError:
-        print('\n\nInvalid Action. Try Again.\n\n')
-        present_actions()
 
 
 def view_movie_queues():
@@ -107,23 +57,9 @@ def add_to_queue():
 
 
 def delete_from_queue():
-    names = member_list()
-    queue_prompt = '\n\nWhich queue do you want to delete from? Options are: '
-    for i in range(len(names)):
-        queue_prompt += names[i]
-        if i != len(names) - 1:
-            queue_prompt += ', '
-    print(queue_prompt)
-    selection = input()
-    while selection not in names:
-        print('\nInvalid queue selection.')
-        print(queue_prompt)
-        selection = input()
-    print('\nGreat! We will use ' + selection + "'s queue to delete movies.")
-    c.execute('SELECT member_ID FROM Members WHERE name = ?;', (selection,))
-    member_ID = c.fetchone()[0]
     movies_to_delete = []
-    movie_prompt = '\nEnter the name of a movie to delete (press enter to exit):'
+    movie_prompt = '\n\nEnter the name of a movie to delete (press enter to ex\
+                    it):'
     print(movie_prompt)
     movie = input()
     while movie != '':
@@ -144,7 +80,8 @@ def delete_from_queue():
 
 def add_to_watched():
         movies_to_update = []
-        movie_prompt = '\nEnter the name of a movie you watched (press enter to exit):'
+        movie_prompt = '\nEnter the name of a movie you watched (press enter t\
+                        o exit):'
         print(movie_prompt)
         movie = input()
         while movie != '':
@@ -177,7 +114,8 @@ def view_watched():
 
 def delete_from_watched():
     movies_to_delete = []
-    movie_prompt = '\nEnter the name of a movie to delete (press enter to exit):'
+    movie_prompt = '\nEnter the name of a movie to delete (press enter to exit\
+                    ):'
     print(movie_prompt)
     movie = input()
     while movie != '':
@@ -250,8 +188,54 @@ def update_the_picker():
     if curr_id + 1 > m_count:
         c.execute('UPDATE ProgramInfo SET current_selector = ?;', (1,))
     else:
-        c.execute('UPDATE ProgramInfo SET current_selector = ?;', (curr_id + 1,))
+        c.execute(
+            'UPDATE ProgramInfo SET current_selector = ?;', (curr_id + 1,))
     conn.commit()
+
+
+def goodbye():
+    conn.commit()
+    conn.close()
+    print('Goodbye!')
+
+
+action_dict = {1: view_movie_queues, 2: add_to_queue, 3: delete_from_queue,
+               4: add_to_watched, 5: view_watched, 6: delete_from_watched,
+               7: view_club_members, 8: show_club_status,
+               9: goodbye}
+
+
+def get_action(name):
+    action_dict.get(name, lambda: 'Invalid')()
+
+
+def present_actions():
+    actions = ['View Movie Queues', 'Add to a Queue', 'Delete from a Queue',
+               'Add Movie to Watched List', 'View Watched List',
+               'Delete from Watched List', 'View Club Members',
+               'Whose Turn Is It?', 'Exit the Program']
+    menu = '\n\nSelect an Action:\n-----------------\n'
+    n = 1
+    for action in actions:
+        menu += str(n) + ' - ' + action + '\n\n'
+        n += 1
+    menu += '\nAction:'
+    print(menu)
+    try:
+        action = int(input())
+        if action not in range(1, n):
+            raise ValueError
+        get_action(action)
+    except ValueError:
+        print('\n\nInvalid Action. Try Again.\n\n')
+        present_actions()
+
+
+def main():
+    with open('greeting_string.txt') as file:
+        greeting = file.read()
+    print('\n\n\n\n\n' + greeting)
+    present_actions()
 
 
 main()
